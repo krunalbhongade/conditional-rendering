ed12aac7fec381bcd1339fd00ef8b54dacce4ebf
Krunal <126875304+krunalbhongade@users.noreply.github.com>
2023-09-14 20:59:59 +0530

    Initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Input(props) {
+  return;
+  <input type="text" placeholder="Username" />;
+  // <input type="password" placeholder="Password" />
+}
+
+export default Input;
